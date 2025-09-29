97ddc451202b5142744e655c2f8525794a3b2584
Magno Fernando da Fonseca <magno.cat@gmail.com>
2025-09-29 00:45:09 -0300

    modified:   src/main/java/com/magnocat/mctrilhas/web/HttpApiManager.java

--- src/main/java/com/magnocat/mctrilhas/web/HttpApiManager.java
+++ src/main/java/com/magnocat/mctrilhas/web/HttpApiManager.java
@@ -40,6 +40,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 // External Libraries
 import com.auth0.jwt.JWT;
